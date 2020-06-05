3c9e856c7fd10b945d7d18491b4232ffb8d5ea7f
VolodymyrMintenko <mintenko.vova@gmail.com>
2020-06-05 22:14:51 +0300

    diploma 12331213123

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
-var cors = require("cors");
+
 const authRoute = require("./routes/auth");
 const campaignsRoute = require("./routes/campaigns");
 
@@ -25,6 +25,17 @@ function createServer() {
   const app = express();
 
   // Allow CORS in development mode
+  if (process.env.NODE_ENV === "development") {
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, OPTIONS"
+      );
+      next();
+    });
+  }
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.json());
@@ -35,36 +46,37 @@ function createServer() {
 
   const port = process.env.PORT || 5000;
   app.listen(port, () => console.log(`Serving on http://localhost:${port}`));
-
-  app.use(cors());
-  let broadcaster;
-
-  const http = require("http");
-  const server = http.createServer(app);
-
-  const io = require("socket.io")(server);
-  app.use(express.static(__dirname + "/public"));
-
-  io.sockets.on("error", (e) => console.log(e));
-  io.sockets.on("connection", (socket) => {
-    socket.on("broadcaster", () => {
-      broadcaster = socket.id;
-      socket.broadcast.emit("broadcaster");
-    });
-    socket.on("watcher", () => {
-      socket.to(broadcaster).emit("watcher", socket.id);
-    });
-    socket.on("offer", (id, message) => {
-      socket.to(id).emit("offer", socket.id, message);
-    });
-    socket.on("answer", (id, message) => {
-      socket.to(id).emit("answer", socket.id, message);
-    });
-    socket.on("candidate", (id, message) => {
-      socket.to(id).emit("candidate", socket.id, message);
-    });
-    socket.on("disconnect", () => {
-      socket.to(broadcaster).emit("disconnectPeer", socket.id);
-    });
-  });
 }
+
+const app = express();
+
+let broadcaster;
+
+const http = require("http");
+const server = http.createServer(app);
+
+const io = require("socket.io")(server);
+app.use(express.static(__dirname + "/public"));
+
+io.sockets.on("error", (e) => console.log(e));
+io.sockets.on("connection", (socket) => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
